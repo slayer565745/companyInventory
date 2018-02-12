de1ba3a6d90c230dc073cdb848dd8f2d02510753
Carson Freeman <carson.freeman@mullenmail.com>
2018-02-12 08:57:39 -0700

    Set up for tonight, going to add employee questions and such.

--- Company Inventory/src/employeeMenu.java
+++ Company Inventory/src/employeeMenu.java
@@ -1,7 +1,16 @@
+import java.util.*;
+
+
 public class employeeMenu
 	{
 		public static void main(String[] args)
 			{
+				//if employee == true
+				ArrayList<String> cmds = new ArrayList<String>();
+				System.out.println("What would you like to do today?");
+				Scanner c = new Scanner(System.in);
+				int choice = c.nextInt();
+				
 				
 			}
 	}

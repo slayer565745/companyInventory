19e7dcd46696a461f5abda03d658f49b129e4919
jialuo.wang <jialuo.wang@mullenmail.com>
2018-02-14 08:09:37 -0700

    changing

--- Company Inventory/src/searching.java
+++ Company Inventory/src/searching.java
@@ -5,13 +5,18 @@ public class searching
 	{
 		public searching(String ui)
 			{
-				ArrayList <String> catagory = new ArrayList <String>();
+				ArrayList <Item> catagory = new ArrayList <Item>();
 				for(int n = 0; n < ArrayList.size(); n++)
 					{
-						if(ArrayList.get(n).indexOf(ui) >= 0)
+						if(ArrayList.get(n).getName().indexOf(ui) >= 0)
 							{
 								catagory.add(ArrayList.get(n));
 							}
+					}
+				System.out.println("Do you mean?");
+				for(int a = 1; a < catagory.size()+1; a++)
+					{
+						System.out.println(a + ")  " + catagory.get(a-1));
 					}
 				
 			}

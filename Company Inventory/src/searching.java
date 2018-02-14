0157be1f46b225aa6f3273c5fab949f8d4649263
jialuo.wang <jialuo.wang@mullenmail.com>
2018-02-14 08:42:30 -0700

    searching might work

--- Company Inventory/src/searching.java
+++ Company Inventory/src/searching.java
@@ -1,10 +1,12 @@
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class searching
 	{
 		public searching(String ui)
 			{
+				int userChoice = 0;
 				ArrayList <Item> catagory = new ArrayList <Item>();
 				for(int n = 0; n < ArrayList.size(); n++)
 					{
@@ -16,8 +18,19 @@ public class searching
 				System.out.println("Do you mean?");
 				for(int a = 1; a < catagory.size()+1; a++)
 					{
-						System.out.println(a + ")  " + catagory.get(a-1));
+						System.out.println(a + ")  " + catagory.get(a-1).getName());
 					}
+				Scanner userinput = new Scanner(System.in);
+				try
+					{
+						userChoice = userinput.nextInt();
+						
+					}
+				catch(InputMismatchException e)
+					{
+						System.out.println("that is not an option");
+					}
+				String itemName = catagory.get(userChoice - 1).getName();
 				
 			}
 
